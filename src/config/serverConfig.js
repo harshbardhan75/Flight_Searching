75eceea0f9dd50c69cf4a8aa3b47598aba07628d
Harsh Kumar <harshbardhan7546@gmail.com>
2025-08-09 10:41:13 +0530

    i have use config folder to import env folder

---
+++ src/config/serverConfig.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config({
+  path: path.resolve(__dirname, '../../.env') // goes up twice: from config -> src -> Searching_micro
+});
+
+module.exports = {
+  PORT: process.env.PORT
+};
